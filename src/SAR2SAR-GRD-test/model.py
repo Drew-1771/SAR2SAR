95f1e632535e27b460bbecd038893cc0b52d72e8
Drew <drewsyp@gmail.com>
2024-02-06 17:22:26 -0800

    Updated code compatability; Now runs from a callable function: run_model in model.py

--- src/SAR2SAR-GRD-test/model.py
+++ src/SAR2SAR-GRD-test/model.py
@@ -1,20 +1,28 @@
+from glob import glob
+
 from utils import *
 from u_net import *
 
-class denoiser(object):
-    def __init__(self, sess, input_c_dim=1):
+
+class Denoiser(object):
+    def __init__(self, sess, input_c_dim=1, debug=True):
         self.sess = sess
         self.input_c_dim = input_c_dim
-        self.Y_ = tf.placeholder(tf.float32, [None, None, None, self.input_c_dim],
-                                 name='clean_image')
+        self.Y_ = tf.compat.v1.placeholder(
+            tf.float32, [None, None, None, self.input_c_dim], name="clean_image"
+        )
         self.Y = autoencoder((self.Y_))
-        init = tf.global_variables_initializer()
+        init = tf.compat.v1.global_variables_initializer()
         self.sess.run(init)
-        print("[*] Initialize model successfully...")
+
+        self.debug = debug
+        if self.debug:
+            print("[*] Initialize model successfully...")
 
     def load(self, checkpoint_dir):
-        print("[*] Reading checkpoint...")
-        saver = tf.train.Saver()
+        if self.debug:
+            print(f"[*] Reading checkpoint from {checkpoint_dir}...")
+        saver = tf.compat.v1.train.Saver()
         ckpt = tf.train.get_checkpoint_state(checkpoint_dir)
         if ckpt and ckpt.model_checkpoint_path:
             full_path = tf.train.latest_checkpoint(checkpoint_dir)
@@ -23,6 +31,8 @@ class denoiser(object):
         else:
             return False
 
+    # original function from https://gitlab.telecom-paris.fr/ring/sar2sar
+    '''
     def test(self, test_files, ckpt_dir, save_dir, dataset_dir, stride):
         """Test SAR2SAR"""
         tf.initialize_all_variables().run()
@@ -73,3 +83,107 @@ class denoiser(object):
             print("Denoised image %s" % imagename)
 
             save_sar_images(outputimage, noisyimage, imagename, save_dir)
+    '''
+
+    def run(
+        self,
+        input_files,
+        save_dir,
+        ckpt_dir,
+        stride,
+        store_noisy: bool,
+        generate_png: bool,
+    ):
+        tf.compat.v1.initialize_all_variables().run()
+        assert len(input_files) != 0, "No testing data!"
+        load_model_status = self.load(ckpt_dir)
+        assert load_model_status == True, "[!] Load weights FAILED..."
+        if self.debug:
+            print("[*] Load weights SUCCESS...")
+            print("[*] start testing...")
+
+        for idx in range(len(input_files)):
+            real_image = load_sar_images(input_files[idx]).astype(np.float32) / 255.0
+            # scan on image dimensions
+            pat_size = 256
+            # Pad the image
+            im_h = np.size(real_image, 1)
+            im_w = np.size(real_image, 2)
+
+            count_image = np.zeros(real_image.shape)
+            output_clean_image = np.zeros(real_image.shape)
+
+            if im_h == pat_size:
+                x_range = list(np.array([0]))
+            else:
+                x_range = list(range(0, im_h - pat_size, stride))
+                if (x_range[-1] + pat_size) < im_h:
+                    x_range.extend(range(im_h - pat_size, im_h - pat_size + 1))
+
+            if im_w == pat_size:
+                y_range = list(np.array([0]))
+            else:
+                y_range = list(range(0, im_w - pat_size, stride))
+                if (y_range[-1] + pat_size) < im_w:
+                    y_range.extend(range(im_w - pat_size, im_w - pat_size + 1))
+
+            for x in x_range:
+                for y in y_range:
+                    tmp_clean_image = self.sess.run(
+                        [self.Y],
+                        feed_dict={
+                            self.Y_: real_image[
+                                :, x : x + pat_size, y : y + pat_size, :
+                            ]
+                        },
+                    )
+                    output_clean_image[:, x : x + pat_size, y : y + pat_size, :] = (
+                        output_clean_image[:, x : x + pat_size, y : y + pat_size, :]
+                        + tmp_clean_image
+                    )
+                    count_image[:, x : x + pat_size, y : y + pat_size, :] = count_image[
+                        :, x : x + pat_size, y : y + pat_size, :
+                    ] + np.ones((1, pat_size, pat_size, 1))
+            output_clean_image = output_clean_image / count_image
+
+            noisyimage = denormalize_sar(real_image)
+            outputimage = denormalize_sar(output_clean_image)
+            if self.debug:
+                print("[*] Denoised image %s" % input_files[idx])
+            save_sar_images(
+                outputimage,
+                noisyimage,
+                str(Path(input_files[idx]).name),
+                save_dir,
+                store_noisy,
+                generate_png,
+                self.debug,
+            )
+
+
+def run_model(
+    input_dir: str,
+    save_dir: str,
+    checkpoint_dir: str = None,
+    stride=64,
+    store_noisy=False,
+    generate_png=False,
+    debug=True,
+):
+    if checkpoint_dir == None:
+        checkpoint_dir = Path(__file__).parent / "checkpoint" / "grd_checkpoint"
+
+    input_files = glob(str(Path(input_dir) / "*.npy").format("float32"))
+    gpu_options = tf.compat.v1.GPUOptions(per_process_gpu_memory_fraction=0.9)
+    with tf.compat.v1.Session(
+        config=tf.compat.v1.ConfigProto(gpu_options=gpu_options)
+    ) as sess:
+        model = Denoiser(sess, debug=debug)
+        model.run(
+            input_files=input_files,
+            save_dir=save_dir,
+            ckpt_dir=checkpoint_dir,
+            stride=stride,
+            store_noisy=store_noisy,
+            generate_png=generate_png,
+        )
--- src/SAR2SAR-GRD-test/utils.py
+++ src/SAR2SAR-GRD-test/utils.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 from PIL import Image
 import scipy.ndimage
 from scipy import special
+from pathlib import Path
 
 
 # DEFINE PARAMETERS OF SPECKLE AND NORMALIZATION FACTOR
@@ -12,11 +13,16 @@ L = 1
 c = (1 / 2) * (special.psi(L) - np.log(L))
 cn = c / (M - m)  # normalized (0,1) mean of log speckle
 
+
 def normalize_sar(im):
-    return ((np.log(np.clip(im,0.24,np.max(im))) - m) * 255 / (M - m)).astype('float32')
+    return ((np.log(np.clip(im, 0.24, np.max(im))) - m) * 255 / (M - m)).astype(
+        "float32"
+    )
+
 
 def denormalize_sar(im):
-    return np.exp((M - m) * (np.squeeze(im)).astype('float32') + m)
+    return np.exp((M - m) * (np.squeeze(im)).astype("float32") + m)
+
 
 def load_sar_images(filelist):
     if not isinstance(filelist, list):
@@ -30,20 +36,58 @@ def load_sar_images(filelist):
         data.append(np.array(im).reshape(1, np.size(im, 0), np.size(im, 1), 1))
     return data
 
+
 def store_data_and_plot(im, threshold, filename):
     im = np.clip(im, 0, threshold)
     im = im / threshold * 255
-    im = Image.fromarray(im.astype('float64')).convert('L')
-    im.save(filename.replace('npy','png'))
+    im = Image.fromarray(im.astype("float64")).convert("L")
+    # check to see if file exists
+    filename = filename.replace("npy", "png")
+    if not Path(filename).exists():
+        Path(filename).touch(exist_ok=True)
+    im.save(filename)
 
 
-def save_sar_images(denoised, noisy, imagename, save_dir):
-    threshold = np.mean(noisy)+3*np.std(noisy)
+def save_sar_images(
+    denoised,
+    noisy,
+    imagename,
+    save_dir,
+    store_noisy: bool,
+    generate_png: bool,
+    debug: bool,
+):
+    if not Path(save_dir).exists():
+        Path(save_dir).mkdir()
+    if debug:
+        print(f"[*] Saving images to {save_dir}")
 
-    denoisedfilename = save_dir + "/denoised_" + imagename
+    threshold = np.mean(noisy) + 3 * np.std(noisy)
+    if threshold == 0:
+        generate_png = False
+        print("[!] Threshold calculated to be 0, could not store as PNG properly")
+
+    denoisedfilename = Path(save_dir) / str("denoised_" + imagename)
+    if not denoisedfilename.exists():
+        denoisedfilename.touch(exist_ok=True)
+    denoisedfilename = str(denoisedfilename)
     np.save(denoisedfilename, denoised)
-    store_data_and_plot(denoised, threshold, denoisedfilename)
+    if debug:
+        print(f"[*] Saved to {denoisedfilename}")
+    if generate_png:
+        store_data_and_plot(denoised, threshold, denoisedfilename)
+        if debug:
+            print(f"[*] Saved png of {denoisedfilename.replace('npy', 'png')}")
 
-    noisyfilename = save_dir + "/noisy_" + imagename
-    np.save(noisyfilename, noisy)
-    store_data_and_plot(noisy, threshold, noisyfilename)
+    if store_noisy:
+        noisyfilename = Path(save_dir) / str("noisy_" + imagename)
+        if not noisyfilename.exists():
+            noisyfilename.touch(exist_ok=True)
+        noisyfilename = str(noisyfilename)
+        np.save(noisyfilename, noisy)
+        if debug:
+            print(f"[*] Saved to {noisyfilename}")
+        if generate_png:
+            store_data_and_plot(noisy, threshold, noisyfilename)
+            if debug:
+                print(f"[*] Saved png of {noisyfilename.replace('npy', 'png')}")
